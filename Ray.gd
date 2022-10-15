e855789400354af25b1095995373ac930ab7e69f
numero15 <nathanaeltardif@gmail.com>
2022-10-15 17:31:32 +0200

    add lock line FX

---
+++ Ray.gd
@@ -0,0 +1,18 @@
+extends Spatial
+
+export var r = 2.0
+export var theta = .5
+export var phi = 1.0
+var _ori = Vector3.ZERO
+
+func _ready():
+	_ori = Vector3(r*sin(phi)*cos(theta),r*sin(phi)*sin(theta),r*cos(phi))
+	transform.origin = _ori
+
+func _process(delta):
+	theta = theta+1*delta
+	phi = phi+1*delta
+	_ori = Vector3(r*sin(phi)*cos(theta),r*sin(phi)*sin(theta),r*cos(phi))
+	transform.origin = _ori
+	_ori = _ori.normalized()
+	look_at(get_parent().global_transform.origin,Vector3.UP)
